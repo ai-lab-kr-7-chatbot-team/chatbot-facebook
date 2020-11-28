993b6972888d54e292e33fc594a19993011564a5
myName <myemail@gmail.com>
2020-11-28 22:59:28 +0900

    telegram_listen post

--- flask_api_demo/__init__.py
+++ flask_api_demo/__init__.py
@@ -14,7 +14,7 @@ telelgram_token = f.readline()
 telelgram_token = telelgram_token.rstrip(' \t\n\r')
 f.close()
 
-print('telelgram_token= [',telelgram_token,']')
+# print('telelgram_token= [',telelgram_token,']')
 
 
 def create_app():
@@ -48,8 +48,9 @@ def create_app():
         print('telegram called webhook  post')
         payload = request.get_json()
         # payload = {'update_id': 280974475, 'message': {'message_id': 31, 'from': {'id': 43446854, 'is_bot': False, 'first_name': 'Roy', 'last_name': 'Cho', 'username': 'roy_cho', 'language_code': 'ko'}, 'chat': {'id': 43446854, 'first_name': 'Roy', 'last_name': 'Cho', 'username': 'roy_cho', 'type': 'private'}, 'date': 1606571726, 'text': 'hi'}}
+        chatMsg = payload['message']['text']
+        print('chatMsg =' ,chatMsg)
 
-        print('payload =' ,payload['message']['text'])
         #const chatMsg = rq.message.text;
         chatMsg = 'test msg'
         url = "https://api.telegram.org/bot"+ telelgram_token + "/sendMessage?chat_id=43446854&text=your answer is :" + chatMsg
